d2ff37914a0bc0d8b8bc4c9c975bca5a8e2afd70
Tao Qian <taoqian_2015@depauw.edu>
2013-10-06 21:25:17 -0400

    Completed most of chapter 9.

--- InterviewPrep/Util.cs
+++ InterviewPrep/Util.cs
@@ -17,5 +17,33 @@ namespace InterviewPrep
                 Console.WriteLine();
             }
         }
+
+        public static void Print2DArray(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                    Console.Write((a[i,j]) + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintArray(List<String> a)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine(a.ElementAt(i));
+            }
+        }
+
+        public static void PrintSet(HashSet<int> s)
+        {
+            Console.Write("{ ");
+            foreach(int i in s)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine("}");
+        }
     }
 }
---
+++ InterviewPrep/chapter09/question0907.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.chapter08
+{
+    class question0907
+    {
+        static void fillColor(int[,] map, int x, int y, int originalColor, int color)
+        {
+            if (x < 0 || y < 0 || x >= map.GetLength(0) || y >= map.GetLength(1))
+                return;
+            if (map[x, y] != originalColor)
+                return;
+            map[x, y] = color;
+            fillColor(map, x + 1, y, originalColor, color);
+            fillColor(map, x + 1, y + 1, originalColor, color);
+            fillColor(map, x - 1, y, originalColor, color);
+            fillColor(map, x - 1, y - 1, originalColor, color);
+            return;
+        }
+    }
+}
